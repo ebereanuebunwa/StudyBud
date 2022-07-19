119b5e7bd1ae773a30aca470013a152a07a345d6
ebereanuebunwa <70179219+ebereanuebunwa@users.noreply.github.com>
2022-07-19 22:43:21 +0100

    StudyBud Project Update
    - Create Models for the base application(Room, Topic, Message), inherit the UserModel provided by django
    - Make migrations to database.
    - Create superuser to login to admin panel.
    - Register models on the admin site(admin.py)
    - Create second superuser and populate the database with data entries
      for each model.
    - Create a templates folder in the project base directory(BASE_DIR) that will contain
      common templates. Configure templates in settings.py
    - Create a templates folder inside the app directory which will house
      all baseapp templates.
    - Use MVT method to implement basic CRUD Functionality on the app.
    - All views are function based views.
    - We can now create, retrieve, update and delete rooms on the site.
    - Stage and commit new files.
    - Push changes to Github

--- studybud/apps/base/models.py
+++ studybud/apps/base/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+# A room will be the child of a topic 
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name  # 
+
+
+# A room will be the child of a topic
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)  # on_delete: when the topic is deleted, 
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True) # nullable and blankable fields 
+    # participants = models.ManyToManyField('Participant', blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated_at', '-created_at']
+
+    def __str__(self):
+        return self.name
+
+
+# Each room will have messages 
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  #
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)  # if a room gets deleted, all messages will be deleted.
+    body = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[:50] + '...'
--- studybud/apps/base/views.py
+++ studybud/apps/base/views.py
@@ -1,11 +1,56 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
 
 # Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Let\'s learn Python!'},
+    {'id': 2, 'name': 'Let\'s learn Django!'},
+    {'id': 3, 'name': 'Let\'s learn Django REST Framework!'},
+    {'id': 4, 'name': 'Code with me.'},
+    {'id': 5, 'name': 'Frontend Development with React!'},
+]
+
+
 def home(request):
-    return HttpResponse('Home page')
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('Room')
+def room(request, pk):
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
 
+def create_room(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()  # save the form to the database
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def delete_room(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete_room.html', {'obj': room})
